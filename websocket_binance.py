aa95be76b1b24216843c057bd1cdef7c985fa205
lbwnb <2781@>
2025-01-02 15:10:40 +0800

    1

--- websocket_binance.py
+++ websocket_binance.py
@@ -83,7 +83,8 @@ def on_open(ws):
     for symbol in symbol_all_usdt:
         # symbol = 'BTCUSDT'
         obj['params'].append(f'{symbol.lower()}_perpetual@continuousKline_5m')
-
+        if len(obj['params']) == 200:
+            break
 
     ws.send(json.dumps(obj))
     print("### open ###")
