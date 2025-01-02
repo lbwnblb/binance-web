0b3b668224b7dcbf265d757696d272fcb4de6b88
lbwnb <2781@>
2025-01-02 14:38:43 +0800

    1

---
+++ utils.py
@@ -0,0 +1,46 @@
+import platform
+import os
+import time
+
+system = platform.system()
+import datetime
+
+intervals = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M']
+
+def convert_to_float(d):
+    for key, value in d.items():
+        if isinstance(value, str) and value.replace('.', '', 1).replace('-', '', 1).isdigit():
+            d[key] = float(value)
+        elif isinstance(value, dict):
+            convert_to_float(value)
+
+
+def get_current_timestamp_ms():
+    return int(time.time() * 1000)
+
+
+def convert_timestamp_to_date(timestamp_ms):
+    # Convert to seconds
+    timestamp_s = timestamp_ms / 1000
+
+    # Convert to datetime
+    date_time = datetime.datetime.fromtimestamp(timestamp_s)
+
+    # Format the datetime
+    formatted_date = date_time.strftime('%Y-%m-%d %H:%M:%S')
+
+    return formatted_date
+if system == "Windows":
+    # # 设置代理地址和端口
+    proxy = "http://127.0.0.1:10808"  # 替换为实际的代理地址和端口
+    os.environ['HTTP_PROXY'] = proxy
+    os.environ['HTTPS_PROXY'] = proxy
+if __name__ == '__main__':
+    v = {'e': 'continuous_kline', 'E': '2025-01-02 13:00:16', 'ps': 'AMBUSDT', 'ct': 'PERPETUAL',
+     'k': {'t': '2025-01-02 13:00:00', 'T': '2025-01-02 13:04:59', 'i': '5m', 'f': 6306341673087, 'L': 6306344126600,
+           'o': '0.0076420', 'c': '0.0076400', 'h': '0.0076450', 'l': '0.0076380', 'v': '262761', 'n': 47, 'x': False,
+           'q': '2007.7661670', 'V': '77258', 'Q': '590.2733980', 'B': '0'}}
+
+    convert_to_float(v)
+    print(v)
+
---
+++ websocket_binance.py
@@ -0,0 +1,98 @@
+import json
+
+import requests
+import websocket
+
+import utils
+binance_fapi = 'https://fapi.binance.com'
+symbol_all_usdt = []
+def init():
+    url = binance_fapi + '/fapi/v1/exchangeInfo'
+    exchangeInfos = requests.get(url).json()
+    for exchangeInfo in exchangeInfos['symbols']:
+        symbol =  exchangeInfo['symbol']
+        if 'USDT' in symbol:
+            symbol_all_usdt.append(symbol)
+# {
+#   "e":"continuous_kline",	// 事件类型
+#   "E":1607443058651,		// 事件时间
+#   "ps":"BTCUSDT",			// 标的交易对
+#   "ct":"PERPETUAL",			// 合约类型
+#   "k":{
+#     "t":1607443020000,		// 这根K线的起始时间
+#     "T":1607443079999,		// 这根K线的结束时间
+#     "i":"1m",				// K线间隔
+#     "f":116467658886,		// 这根K线期间第一笔更新ID
+#     "L":116468012423,		// 这根K线期间末一笔更新ID
+#     "o":"18787.00",			// 这根K线期间第一笔成交价
+#     "c":"18804.04",			// 这根K线期间末一笔成交价
+#     "h":"18804.04",			// 这根K线期间最高成交价
+#     "l":"18786.54",			// 这根K线期间最低成交价
+#     "v":"197.664",			// 这根K线期间成交量
+#     "n":543,				// 这根K线期间成交笔数
+#     "x":false,				// 这根K线是否完结(是否已经开始下一根K线)
+#     "q":"3715253.19494",	// 这根K线期间成交额
+#     "V":"184.769",			// 主动买入的成交量
+#     "Q":"3472925.84746",	// 主动买入的成交额
+#     "B":"0"					// 忽略此参数
+#   }
+# }
+
+change = {'next': utils.get_current_timestamp_ms()}
+
+current_interval = '5m'
+
+def change_init():
+    for interval in utils.intervals:
+        change[interval] = {}
+
+
+
+def on_message(ws, message):
+    message = json.loads(message)
+    utils.convert_to_float(message)
+    k = message['k']
+    symbol = message['ps']
+    i = k['i']
+    # print(f"{symbol} {k['i']}  开始时间:{utils.convert_timestamp_to_date(k['t'])}      涨幅:{round((k['c']-k['o'])/k['o']*100,2)}%")
+    change[i][symbol] = round((k['c']-k['o'])/k['o']*100,2)
+
+
+    if message['E'] > change['next']:
+        change['next'] = message['E'] + 1000*5
+        for change_key in change.keys():
+            if change_key in current_interval:
+                interval_map = change[change_key]
+                sorted_interval_map = sorted(interval_map.items(), key=lambda x: x[1], reverse=True)
+                print(f"开始时间:{utils.convert_timestamp_to_date(k['t'])}")
+                for interval_map_key in sorted_interval_map.keys():
+                    print(f"{interval_map_key} {change_key} 涨幅:{sorted_interval_map[interval_map_key]}%")
+
+
+def on_error(ws, error):
+    pass
+def on_close(ws,close_status_code, close_msg):
+    pass
+
+def on_open(ws):
+    obj = {
+        "method": "SUBSCRIBE"
+        ,"params":[]
+        , "id": 1
+    }
+
+    for symbol in symbol_all_usdt:
+        obj['params'].append(f'{symbol.lower()}@continuousKline_5m')
+
+
+    ws.send(json.dumps(obj))
+    print("### open ###")
+
+
+
+if __name__ == '__main__':
+    init()
+    # ws = websocket.WebSocketApp("wss://fstream.binance.com/ws", on_close=on_close, on_open=on_open,
+    #                             on_message=on_message,
+    #                             on_error=on_error)
+    # ws.run_forever()
