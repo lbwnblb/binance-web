6d5127845cc5aceeb84d1ae9f7dcdf42592ba0d0
lbwnb <2781@>
2025-01-11 20:21:07 +0800

    1

--- websocket_binance.py
+++ websocket_binance.py
@@ -165,7 +165,7 @@ if __name__ == '__main__':
                 symbol_filter.append(symbol)
     klines_map = {}
     for symbol in symbol_filter:
-        klines_map[symbol] = klines(symbol,'15m',limit=100)
+        klines_map[symbol] = klines(symbol,'15m',limit=24*4*3)
 
     avg_map = {}
     for key in klines_map.keys():
