4edd80a787f6d13f9cb02f9eee4e51b2106628c5
lbwnb <2781@>
2025-01-11 20:56:01 +0800

    1

--- websocket_binance.py
+++ websocket_binance.py
@@ -181,12 +181,15 @@ if __name__ == '__main__':
         if last:
             item = [item for item in avg_map[key] if item['symbol'] == last['symbol']][0]
             if last['side'] in 'BUY' and item['open'] > item['low']:
-                print('收益:',item['change'])
-                total_income += item['change']
+                flag =  (utils.price_change(item['open'],item['low']) < -1)
+
+                print('收益:','-1' if flag else item['change'])
+                total_income += (-1 if flag else item['change'])
             else:
                 if item['open'] < item['high']:
-                    print('收益:',-item['change'])
-                    total_income -= item['change']
+                    flag = (utils.price_change(item['open'], item['high']) > 1)
+                    print('收益:','-1' if flag else -item['change'])
+                    total_income -= (-1 if flag else item['change'])
 
         sum_number = sum([item['change'] for item in avg_map[key]])
         sum_change = round(sum_number / len(avg_map[key]), 2)
